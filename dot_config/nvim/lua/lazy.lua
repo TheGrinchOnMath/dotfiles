7ff9611a59d78b01982d6442c064bc9aafb1f584
TheGrinchOnMath <103316040+TheGrinchOnMath@users.noreply.github.com>
2025-06-17 15:26:24 +0200

    switching to alacritty

--- dot_config/nvim/lua/lazy.lua
+++ dot_config/nvim/lua/lazy.lua
@@ -11,58 +11,31 @@ end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 -- [[ Configure and install plugins ]]
---
 --  To check the current status of your plugins, run
 --    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
-if vim.g.vscode then
-  require('lazy').setup({
-    -- import all plugins --
-    require 'plugins.mini',
-    require 'plugins.comment',
-    require 'plugins.gruvbox',
-    require 'plugins.lazydev',
-    -- use when working with hex code colors
-    -- require 'plugins.nvim-colorizer',
-    -- require("plugins.nvim-treesitter"),
-    require 'plugins.which-key',
-    require 'plugins.todo-comments',
-  }, {
-    ui = {
-      -- If you are using a Nerd Font: set icons to an empty table which will use the
-      -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-      icons = vim.g.have_nerd_font and {} or {
-        cmd = '⌘',
-        config = '🛠',
-        event = '📅',
-        ft = '📂',
-        init = '⚙',
-        keys = '🗝',
-        plugin = '🔌',
-        runtime = '💻',
-        require = '🌙',
-        source = '📄',
-        start = '🚀',
-        task = '📌',
-        lazy = '💤 ',
-      },
-    },
-  })
-else
-  require('lazy').setup({
+
+-- NOTE: this condition is necessary if using the nvim-vscodium plugin
+local plugins = vim.g.vscode
+    and {
+      -- import all plugins --
+      require 'plugins.mini',
+      require 'plugins.comment',
+      require 'plugins.gruvbox',
+      require 'plugins.lazydev',
+      -- use when working with hex code colors
+      -- require 'plugins.nvim-colorizer',
+      -- require("plugins.nvim-treesitter"),
+      require 'plugins.which-key',
+      require 'plugins.todo-comments',
+    }
+  or {
     -- try getting this one plugin to work FIXME
     require 'plugins.copilot',
 
     -- import all plugins --
     require 'plugins.alpha',
     require 'plugins.autopairs',
-    require 'plugins.cmp',
+    require 'plugins.blink',
     -- require 'plugins.go',
     require 'plugins.mini',
     require 'plugins.neogit',
@@ -78,6 +51,7 @@ else
     require 'plugins.indent_line',
     require 'plugins.lazydev',
     require 'plugins.markdown-preview',
+    require 'plugins.multiple-cursors',
     require 'plugins.neominimap',
     -- use when working with hex code colors
     -- require 'plugins.nvim-colorizer',
@@ -85,32 +59,30 @@ else
     require 'plugins.nvim-lspconfig',
     require 'plugins.nvim-treesitter',
     require 'plugins.nvim-ufo',
-    require 'plugins.ft-python.f-string-toggle',
+    -- require 'plugins.ft-python.f-string-toggle',
     require 'plugins.telescope',
     require 'plugins.which-key',
     require 'plugins.todo-comments',
+  }
 
-    -- imports all plugins
-    -- { import = 'plugins' },
-  }, {
-    ui = {
-      -- If you are using a Nerd Font: set icons to an empty table which will use the
-      -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-      icons = vim.g.have_nerd_font and {} or {
-        cmd = '⌘',
-        config = '🛠',
-        event = '📅',
-        ft = '📂',
-        init = '⚙',
-        keys = '🗝',
-        plugin = '🔌',
-        runtime = '💻',
-        require = '🌙',
-        source = '📄',
-        start = '🚀',
-        task = '📌',
-        lazy = '💤 ',
-      },
+require('lazy').setup(plugins, {
+  ui = {
+    -- If you are using a Nerd Font: set icons to an empty table which will use the
+    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
     },
-  })
-end
+  },
+})
